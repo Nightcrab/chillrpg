4b0bde3566525cd21d7f484f56d968da3d304894
Nightcrab <paul.kr.anderson@gmail.com>
2018-08-03 01:57:08 +1000

    bugfix flood

--- crpg.py
+++ crpg.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from .utils.dataIO import fileIO
 import random
 import glob
+import pickle
 import re
 
 pi = 3.1415
@@ -12,6 +13,12 @@ pi = 3.1415
 def genID(size=8, chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split()):
     return ''.join(random.choice(chars) for _ in range(size))
 
+def savePickle(filename, obj):
+    filehandler = open(filename, 'wb')
+    pickle.dump(obj, filehandler)
+def loadPickle(filename):
+    filehandler = open(filename, 'rb')
+    return pickle.load(filehandler)
 class Player:
     def __init__(self, info):
         self.ai = AI()
@@ -19,8 +26,15 @@ class Player:
         for key in info:
             setattr(self, key, info[key])
 
+    def isDead(self):
+        if self.health.h <= 0:
+            return True
+        if self.health.HP <= 0:
+            return True
+        return False
+
     async def save(self):
-        fileIO("data/crpg/players/{}/info.json".format(id), "save", self.info)
+        savePickle("data/crpg/players/{}/info.obj".format(self.ID), self)
 
 class Item:
     def __init__(self, itemID="", *info, **kwargs):
@@ -41,29 +55,26 @@ class Item:
 
 class AI:
     def __init__(self, defend=None, action=None):
-        if defend == None:
-            def defend(self, weapon, attack):
-                stab_table = {
-                "la" : "mr",
-                "ra" : "ml",
-                "t" : "pl",
-                "h" : "pr"
-                }
-                strike_table = {
-                "la" : "pl",
-                "ra" : "pr",
-                "t" : "pu",
-                "h" : "pl"
-                }
-                if attack.type == "stab":
-                    return Defense(stab_table[attack.target])
-                elif attack.type == "strike":
-                    return Defense(strike_table[attack.target])
-            self.defend = defend
-        if action == None:
-            def action(self, env):
-                return random.choice(["stab", "strike"])
-            self.action = action
+        return
+    def defend(self, weapon, attack):
+        stab_table = {
+        "la" : "mr", #left arm, move right
+        "ra" : "ml",
+        "t" : "pl", #torso, parry left
+        "h" : "pr"
+        }
+        strike_table = {
+        "la" : "pl",
+        "ra" : "pr",
+        "t" : "pu",
+        "h" : "pl"
+        }
+        if attack.type == "stab":
+            return Defense(stab_table[attack.target])
+        elif attack.type == "strike":
+            return Defense(strike_table[attack.target])
+    def action(self, env):
+        return random.choice(["stab ", "strike "])+random.choice(["h","t"])
 
 class Enemy:
     def __init__(self, info, ai=None):
@@ -84,12 +95,12 @@ class Attack:
         self.distance = distance
         self.weapon = weapon
         if atktype == "stab":
-            self.damage = weapon.blade.mass*player.strength.arm*weapon.blade.stab.sharpness/weapon.blade.stab.area
-
+            self.damage = weapon.blade.mass*player.strength.arm*weapon.blade.stab.sharpness/weapon.blade.stab.area # dodgy as fuck lol I'll make a proper one later but I wanted to get something working because I've been doing no testing except for the character creation stuff
+        return
         # more advanced combat system for future implementation
         if atktype == "strike":
             acceleration = player.strength/weapon.mass
-            self.velocity = acceleration * distance
+            self.velocity = acceleration #insert kinematic equation lol
             if weapon.type == sword:
                 self.attack_area = weapon.length - distance
             else:
@@ -102,13 +113,62 @@ class Defense:
 
 class Fight:
     """Created whenever a player enters a fight."""
-    def __init__(self, player1, player2, bot, channel):
+    def __init__(self, player1, player2, chillrpg, channel):
         self.p1 = player1
         self.p2 = player2
-        self.bot = bot
+        self.p1.pos = -2
+        self.p2.pos = 2
+        self.rpg = chillrpg
         self.channel = channel
-    async def turn(self):
-        return
+
+    async def start(self):
+        description = "you're in a fight, defend yourself m8"
+        embed = discord.Embed(title="{} vs {}".format(self.p1.name, self.p2.name), description=description, color=0x16ff64)
+        embed.add_field(title="Stances", value="{}'s stance:{}\n{}'s stance:".format(self.p1.stance, self.p2.stance), inline=False)
+        
+        if self.p1.isPlayer:
+            h1 = self.p1.health
+            embed.add_field(title="{}'s' Health".format(self.p1.name), value="HP: {}\nHead: {}\nTorso: {}\nLeft Arm: {}\nRight Arm: {}\nLeft Leg: {}\nRight Leg: {}\n".format(h1.HP, h1.h, h1.t, h1.la, h1.ra, h1.ll, h1.rl), inline=False)
+        if self.p2.isPlayer:
+            h2 = self.p2.health
+            embed.add_field(title="{}'s' Health".format(self.p2.name), value="HP: {}\nHead: {}\nTorso: {}\nLeft Arm: {}\nRight Arm: {}\nLeft Leg: {}\nRight Leg: {}\n".format(h2.HP, h2.h, h2.t, h2.la, h2.ra, h2.ll, h2.rl), inline=False)
+        self.message = await self.rpg.bot.send_message(self.channel, embed=embed)
+        self.nextTurn()
+
+    async def resolveAttack(self, attack, defense, attacker, defender):
+        return [attacker, defender]
+
+    async def nextTurn(self):
+        distance = abs(self.p1.pos)+abs(self.p2.pos)
+        if self.p1.isPlayer:
+            action1 = await self.rpg.promptUser(self.bot.get_user_info(self.p1.id), channel)
+        else:
+            action1 = await self.p1.ai.action(self)
+        if self.p2.isPlayer:
+            action2 = await self.rpg.promptUser(self.bot.get_user_info(self.p2.id), channel)
+        else:
+            action2 = await self.p2.ai.action(self)
+        action1 = action1.split(" ")
+        action2 = action2.split(" ")
+        if not action1[0].startsWith("stance"):
+            attack1 = Attack(p1, distance, p1.weapon, action1[0], action1[1])
+        if not action2[0].startsWith("stance"):
+            attack2 = Attack(p2, distance, p2.weapon, action2[0], action2[1])
+        if not attack1 == None:
+            defense2 = self.p2.ai.defend(p2.weapon, attack1)
+            result = await resolveAttack(attack1, defense2, self.p1.health)
+            self.p1 = result[0]
+            self.p2 = result[1]
+        if not attack2 == None and self.p1.alive:
+            defense1 = self.p1.ai.defend(p1.weapon, attack2)
+            result = await resolveAttack(attack1, defense2, self.p2, self.p1)
+            self.p1 = result[1]
+            self.p2 = result[0]
+        if not self.p1.alive or not self.p2.alive:
+            return [self.p1,self.p2]
+        return await self.turn()
+        
+        
 class ChillRPG:
     """Class holding game information, methods and player interaction."""
     def __init__(self, bot):
@@ -134,20 +194,19 @@ class ChillRPG:
 
     async def getPlayer(self, ID, ctx): #Get a user's character, if it exists
         if os.path.exists("data/crpg/players/{}".format(ID)):
-            info = fileIO("data/crpg/players/{}/info.json".format(ID), "load")
-            return Player(info)
+            return loadPickle("data/crpg/players/{}/info.obj".format(ID))
         else:
             prompt = "You haven't created a character yet, or your previous one is deceased. Would you like to create a new character?"
             response = await self.promptUser(ctx.message.author, ctx.message.channel, prompt)
             if response.lower() in yes:
-                return await self.newPlayer(ID, ctx.message.channel)
+                return await self.newPlayer(ctx.message.author, ctx.message.channel)
 
     async def newPlayer(self, user, channel): #Initiate character creation
         ID = user.id
 
         info = fileIO("data/crpg/default_player.json", "load")
         os.makedirs("data/crpg/players/{}".format(ID))
-        fileIO("data/crpg/players/{}/info.json".format(ID), "save", info)
+        savePickle("data/crpg/players/{}/info.obj".format(ID), Player(info))
 
         await self.bot.send_message(channel, "Welcome to character creation.")
         info["ID"] = ID
@@ -167,16 +226,20 @@ class ChillRPG:
         confirm = await self.promptUser(user, channel, "Does the following describe your new character? ```{} is a {} year old {}. They have no skills or particular beneficial character traits.```".format(info["name"], info["age"], info["gender"]))
         if confirm.lower() in self.yes:
             player = Player(info)
-            player.save()
+            await player.save()
             return player
         else:
             await self.bot.send_message(channel, "Restarting character creation...")
             return await self.newPlayer(ID, channel)
 
-    async def promptUser(self, author, channel, prompt):
-        await self.bot.send_message(channel, prompt)
+    async def promptUser(self, author, channel, prompt=None):
+        if not prompt == None:
+            await self.bot.send_message(channel, prompt)
         response = await self.bot.wait_for_message(author=author, channel=channel)
         return response.content
+
+    async def newFight(self, p1, p2, ctx):
+        return Fight(p1, p2, self, ctx.message.channel)
 
     def listItems(self, inv):
         content = ""
@@ -184,16 +247,30 @@ class ChillRPG:
             content += "\n{} x {}".format(item.name, item.amount)
         return content
 
-    def status(self, player):
+    def listStats(self, player):
+        content = "Health: {}\nHead: {}\nTorso: {}\nLeft Arm: {}\nRight Arm: {}\nLeft Leg: {}\nRight Leg: {}\n"
+
+    def statusEmbed(self, player):
         embed = discord.Embed(title="{}'s Status".format(player.name), color=0x16ff64)
-        embed.add_field(name="Stats", value="Health:{}\nStamina:{}\nFatigue:{}\nLevel:{}\nBalance:{}\n".format(player.HP, player.stamina, player.fatigue, player.level, player.balance), inline=False)
+        embed.add_field(name="Stats", value="Health: {}\nStamina: {}\nFatigue: {}\nLevel: {}\nBalance: {}\n".format(player.health["HP"], player.stamina, player.fatigue, player.level, player.balance), inline=False)
         embed.add_field(name="Location", value="{}".format(self.locations[player.location]["description"]), inline=False)
         return embed
 
     @commands.command(pass_context=True)
     async def inventory(self, ctx):
-        embed = discord.Embed(title="Inventory", description=self.listItems(self.getPlayer(ctx.message.author.id)), color=0x16ffeb)
+        embed = discord.Embed(title="Inventory", description=self.listItems((await self.getPlayer(ctx.message.author.id, ctx)).inv), color=0x16ffeb)
         await self.bot.send_message(ctx.message.channel, embed=embed)
+
+    @commands.command(pass_context=True)
+    async def status(self, ctx):
+        player = await self.getPlayer(ctx.message.author.id, ctx)
+        await self.bot.send_message(ctx.message.channel, embed=self.statusEmbed(player))
+
+    @commands.command(pass_context=True)
+    async def save(self, ctx):
+        player = await self.getPlayer(ctx.message.author.id, ctx)
+        await player.save()
+
     @commands.command(pass_context=True)
     async def begin(self, ctx):
         if os.path.isdir("data/crpg/players/{}".format(ctx.message.author.id)):
@@ -204,7 +281,7 @@ class ChillRPG:
                 await self.bot.send_message(ctx.message.channel, "Cancelled character recreation.")
                 return
         player = await self.newPlayer(ctx.message.author, ctx.message.channel)
-        await self.bot.send_message(ctx.message.channel, embed=self.status(player))
+        await self.bot.send_message(ctx.message.channel, embed=self.statusEmbed(player))
 
 def check_folders():
     if not os.path.exists("data/crpg"):
