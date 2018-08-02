72200982cf04e60102fd12ac84d33b7a5ce66ea4
Paul Anderson <paul.kr.anderson@gmail.com>
2018-08-02 14:13:10 +1000

    added character creation

--- crpg.py
+++ crpg.py
@@ -9,11 +9,11 @@ def genID(size=8, chars=string.ascii_lowercase + string.digits):
 
 class Player:
 	def __init__(self, info):
-		
 
 class Item:
 	def __init__(self, itemID=None, *info, **kwargs):
 
+		#if id is given, take the properties of default item with that id, if not, properties should be passed in a dictionary
 		if itemID == None:
 			self.itemID = genID()
 		else:
@@ -50,13 +50,14 @@ class Fight:
 		self.p2 = player2
 
 class ChillRPG:
-	"""Class holding game information and methods."""
+	"""Class holding game information, methods and player interaction."""
 	def __init__(self, bot):
 		self.bot = bot
-		self.yes = ["yes", "y", "ok", "okay"]
+		self.yes = ["yes", "y", "ok", "okay", "yep", "yeah"]
 		self.no = ["no", "n"]
 		self.startkits = [[Item("pitchfork01", 1)],
-						  [Item("steel_axe01", 1)]]
+						  [Item("steel_axe01", 1)],
+						  [Item("steel_spear01", 1)]]
 
 	def refreshInv(self, inv):
 		for x in inv.length:
@@ -80,8 +81,8 @@ class ChillRPG:
         	if response.lower() in yes:
         		return await self.newPlayer(id, ctx)
 
-    async def newPlayer(self, id, channel): #Initiate character creation
-    	user = self.bot.get_user_info(id)
+    async def newPlayer(self, ID, channel): #Initiate character creation
+    	user = self.bot.get_user_info(ID)
 
     	info = fileIO("data/crpg/default_player.json", "load")
         os.makedirs("data/crpg/players/{}".format(id))
@@ -89,10 +90,24 @@ class ChillRPG:
 
     	await self.bot.send_message(channel, "Welcome to character creation.")
     	info["Name"] = await self.promptUser(user, channel, "What will your name be?")
-    	info["Gender"] = await self.promptUser(user, channel, "What will your gender be?")
-    	start_gear = await self.promptUser(user, channel, "What is your background? Respond with a number.\n```0. Peasant\n1. Lumberjack\n2. Deserter```")
+    	info["Age"] = await self.promptUser(user, channel, "How old are you (in years)?")
+    	info["Gender"] = await self.promptUser(user, channel, "What will your gender be?").lower()
+    	history = await self.promptUser(user, channel, "What is your background? Respond with a number.\n```0. Peasant\n1. Lumberjack\n2. Deserter```")
+    	if not history.isdigit():
+    		history = "0"
+    	history = int(history)
     	start_buff = await self.promptUser(user, channel, "What trait would you like to begin with? Respond with a number.\n```0. None```")
 
+    	if history == None or history < 0 or history > len(self.startkits)+1:
+    		history = 0
+    	info["inv"] = self.startkits[history]
+    	confirm = await self.promptUser(user, channel, "Does the following describe your new character? ```{} is a {} year old {}. They have no skills or particular beneficial character traits.```".format(info["Name"], info["Age"], info["Gender"]))
+    	if confirm.lower() in self.yes:
+			player = Player(info)
+			return await self.savePlayer(player)
+		else:
+			await self.bot.send_message(channel, "Restarting character creation...")
+			return await self.newPlayer(ID, channel)
 
     async def promptUser(self, author, channel, prompt):
     	await self.bot.send_message(channel, prompt)
@@ -100,6 +115,7 @@ class ChillRPG:
 
     async def savePlayer(self, player):
     	fileIO("data/crpg/players/{}/info.json".format(id), "save", info)
+    	return player
 
 def check_folders():
     if not os.path.exists("data/crpg"):
