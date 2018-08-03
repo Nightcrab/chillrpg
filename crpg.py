06c4ce3689b63c1f60aa75dd06740f5b16d9fa51
Paul Anderson <paul.kr.anderson@gmail.com>
2018-08-03 14:36:42 +1000

    added materials and combat stuff

--- crpg.py
+++ crpg.py
@@ -7,8 +7,8 @@ import random
 import glob
 import pickle
 import re
+import math
 
-pi = 3.1415
 
 def genID(size=8, chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split()):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -19,6 +19,25 @@ def savePickle(filename, obj):
 def loadPickle(filename):
     filehandler = open(filename, 'rb')
     return pickle.load(filehandler)
+pos = {
+      "h" :  [0,1],
+      "t" :  [0,0],
+      "la":  [1,0],
+      "ra": [-1,0],
+      "ll": [1,-1],
+      "rl":[-1,-1]
+      }
+
+def distance(pos1, pos2):
+    x1 = pos1[0]
+    y1 = pos1[1]
+    z1 = pos1[2]
+    x2 = pos2[0]
+    y2 = pos2[1]
+    z2 = pos2[2]
+    d1 = math.sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1))
+    return math.sqrt(d1*d1+(z2 - z1)*(z2 - z1))
+
 class Player:
     def __init__(self, info):
         self.ai = AI()
@@ -33,11 +52,18 @@ class Player:
             return True
         return False
 
+    def changeEquip(self, itemname):
+        matches = [item for item in self.inv if item.name.lower() == itemname.lower()] 
+        if len(matches) > 0:
+            self.weapon = matches[0]
+        else:
+            self.weapon = [item for item in self.inv if item.name.lower() == "fists"][0]
+
     async def save(self):
         savePickle("data/crpg/players/{}/info.obj".format(self.ID), self)
 
 class Item:
-    def __init__(self, itemID="", *info, **kwargs):
+    def __init__(self, itemID, *info, **kwargs):
 
         #if id is given, take the properties of default item with that id, if not, properties should be passed in a dictionary
         if itemID == "":
@@ -86,30 +112,63 @@ class Enemy:
         else:
             self.ai = ai
 
+class Stance:
+    def __init__(self, player, string):
+        self.code = string.split("")
+        self.x = int(code[0])*0.3 #-1 left 0 middle 1 right
+        self.y = int(code[1])*0.7 #-1 bottom 0 middle 1 top
+        self.z = int(code[2])*player.proportions["arm"]/3 #0 close 1 neutral 2 forward 3 overextended
+
+    def lungelen(self):
+        return 0
+
+class Material:
+    def __init__(self, name, brittle, tensile, toughness, density):
+        self.name = name
+        self.brittle = brittle
+        self.tensile = tensile
+        self.toughness = toughness
+        self.density = density
+
 class Attack:
     """An attack, usually countered with a defense."""
-    def __init__(self, player, distance, weapon, atktype, target):
+    def __init__(self, player, p_distance, weapon, atktype, target):
         self.type = atktype
         self.stance = player.stance
         self.target = target
-        self.distance = distance
+        self.dz = p_distance - self.stance.z
+        self.dx = self.stance.x - pos[target][0]
+        self.dy = self.stance.y - pos[target][1]
+        self.d1 = math.sqrt(dx*dx+dy*dy)
+        self.distance = math.sqrt(dz*dz+d1*d1)
+
         self.weapon = weapon
         if atktype == "stab":
-            self.damage = weapon.blade.mass*player.strength.arm*weapon.blade.stab.sharpness/weapon.blade.stab.area # dodgy as fuck lol I'll make a proper one later but I wanted to get something working because I've been doing no testing except for the character creation stuff
-        return
-        # more advanced combat system for future implementation
+            w_acceleration = player.strength.arm/weapon.mass
+            b_acceleration = player.strength.leg/(player.mass+player.invmass+weapon.mass)
+            self.velocity = math.sqrt(2*w_acceleration*self.distance)
+            self.velocity += math.sqrt(2*b_acceleration*self.stance.lungelen())
+            self.acceleration = w_acceleration + b_acceleration
+            self.attack_area = weapon.blade["stab"]["area"]
+        
         if atktype == "strike":
-            acceleration = player.strength/weapon.mass
-            self.velocity = acceleration #insert kinematic equation lol
+            self.acceleration = player.strength.arm/weapon.mass
+            self.velocity = math.sqrt(2*acceleration*self.distance)
             if weapon.type == sword:
                 self.attack_area = weapon.length - distance
             else:
-                self.attack_area = weapon.blade.strike.area
+                self.attack_area = weapon.blade["strike"]["area"]
+        self.energy = 1/2*mass*velocity*velocity
 
 class Defense:
     """Determines the effectiveness of an attack."""
-    def __init__(self, player, weapon, stance):
+    def __init__(self, player, weapon, stance, attack):
         self.type = player.ai.defend(player.weapon, attack)
+        self.weapon =  weapon
+        self.stance = stance
+        self.reaction = player.combat_stats.reactionspeed*player.buffs.reaction
+        if self.type == "b":
+            self.destination = pos[attack.target]
 
 class Fight:
     """Created whenever a player enters a fight."""
@@ -120,9 +179,10 @@ class Fight:
         self.p2.pos = 2
         self.rpg = chillrpg
         self.channel = channel
+        self.turns = ["The battle began between {} and {}.".format(self.p1.name, self.p2.name)]
 
     def updatemsg(self):
-    	description = "you're in a fight, defend yourself m8"
+        description = "you're in a fight, defend yourself m8"
         embed = discord.Embed(title="{} vs {}".format(self.p1.name, self.p2.name), description=description, color=0x16ff64)
         embed.add_field(title="Stances", value="{}'s stance:{}\n{}'s stance:".format(self.p1.stance, self.p2.stance), inline=False)
         
@@ -132,14 +192,21 @@ class Fight:
         if self.p2.isPlayer:
             h2 = self.p2.health
             embed.add_field(title="{}'s' Health".format(self.p2.name), value="HP: {}\nHead: {}\nTorso: {}\nLeft Arm: {}\nRight Arm: {}\nLeft Leg: {}\nRight Leg: {}\n".format(h2.HP, h2.h, h2.t, h2.la, h2.ra, h2.ll, h2.rl), inline=False)
+        embed.add_field(title="Battle Log", value="\n".join(self.turns), inline=False)
         return embed
 
     async def start(self):
-    	self.message = await self.rpg.bot.send_message(self.channel, embed=self.updatemsg())
+        self.message = await self.rpg.bot.send_message(self.channel, embed=self.updatemsg())
         self.nextTurn()
 
     async def resolveAttack(self, attack, defense, attacker, defender):
-        return [attacker, defender]
+        def success(attack, defense):
+            return True
+        def calcDamage(attack, armour):
+            pressure = attack.weapon.blade[attack.type]["sharpness"]/100*attacker.strength.arm/attack.weapon.blade[attack.type]["area"] # sharpness applied as a multiplier of pressure, effectively dividing the impact area and thus increasing the imapct pressure
+            cut_factor = attack.attack_area*attack.velocity/attack.stance.z
+        if success:
+            defender.stance = Stance(defender, "".join(pos[attack.target]+[0]))
 
     async def nextTurn(self):
         distance = abs(self.p1.pos)+abs(self.p2.pos)
@@ -162,12 +229,12 @@ class Fight:
             result = await resolveAttack(attack1, defense2, self.p1.health)
             self.p1 = result[0]
             self.p2 = result[1]
-        if not attack2 == None and self.p1.alive:
+        if not attack2 == None and not self.p2.isDead():
             defense1 = self.p1.ai.defend(p1.weapon, attack2)
             result = await resolveAttack(attack1, defense2, self.p2, self.p1)
             self.p1 = result[1]
             self.p2 = result[0]
-        if not self.p1.alive or not self.p2.alive:
+        if self.p1.isDead() or self.p2.isDead():
             return [self.p1,self.p2]
         return await self.turn()
         
@@ -182,6 +249,7 @@ class ChillRPG:
         self.startkits = [[Item("pitchfork01", {"amount":1})],
                           [Item("steel_axe01", {"amount":1})],
                           [Item("steel_spear01", {"amount":1})]]
+        self.materials = [Material("mild_steel", True, 70, 370, 8050)]
 
     def refreshInv(self, inv):
         for x in inv.length:
@@ -201,7 +269,7 @@ class ChillRPG:
         else:
             prompt = "You haven't created a character yet, or your previous one is deceased. Would you like to create a new character?"
             response = await self.promptUser(ctx.message.author, ctx.message.channel, prompt)
-            if response.lower() in yes:
+            if response.lower() in self.yes:
                 return await self.newPlayer(ctx.message.author, ctx.message.channel)
 
     async def newPlayer(self, user, channel): #Initiate character creation
@@ -209,7 +277,6 @@ class ChillRPG:
 
         info = fileIO("data/crpg/default_player.json", "load")
         os.makedirs("data/crpg/players/{}".format(ID))
-        savePickle("data/crpg/players/{}/info.obj".format(ID), Player(info))
 
         await self.bot.send_message(channel, "Welcome to character creation.")
         info["ID"] = ID
@@ -226,6 +293,25 @@ class ChillRPG:
         if history == None or history < 0 or history > len(self.startkits)+1:
             history = 0
         info["inv"] = self.startkits[history]
+        fist = {
+            "name":"Fists",
+            "type":"knuckledusters",
+            "length": info["proportions"]["arm"],
+            "mass": 0.0065*info["mass"],
+            "balance":70,
+            "blade": {
+                "mass": 0.0065,
+                "length":0.04,
+                "stab": {
+                    "area" : 5,
+                    "sharpness" : 10
+                }
+            },
+            "attacks": {
+                "stab":"Punch"
+            }
+        }
+        info["inv"].append(Item("", fist, {"amount":1}))
         confirm = await self.promptUser(user, channel, "Does the following describe your new character? ```{} is a {} year old {}. They have no skills or particular beneficial character traits.```".format(info["name"], info["age"], info["gender"]))
         if confirm.lower() in self.yes:
             player = Player(info)
@@ -268,6 +354,12 @@ class ChillRPG:
     async def status(self, ctx):
         player = await self.getPlayer(ctx.message.author.id, ctx)
         await self.bot.send_message(ctx.message.channel, embed=self.statusEmbed(player))
+
+    @commands.command(pass_context=True)
+    async def weapon(self, ctx):
+        player = await self.getPlayer(ctx.message.author.id, ctx)
+        player.changeEquip(ctx.message.content[7:])
+        await self.bot.send_message(ctx.message.channel, embed=discord.Embed(title="Success", description="Set your weapon to {}.".format(player.weapon.name), color=0x16ffeb))
 
     @commands.command(pass_context=True)
     async def save(self, ctx):
